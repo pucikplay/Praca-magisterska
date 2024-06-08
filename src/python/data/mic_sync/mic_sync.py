e2593a602b0e09b720500cd54f87227a209d24a1
Gabriel Budziński <pucikplay@gmail.com>
2024-06-08 11:31:23 +0200

    Append to previous

--- src/python/data/mic_sync/mic_sync.py
+++ src/python/data/mic_sync/mic_sync.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from matplotlib.patches import ConnectionPatch
 
 NODES_NO = 4
 labels = ['0', '1', '2', '3']
@@ -7,19 +8,37 @@ colors = ['r', 'g', 'b', 'y']
 
 
 def plot_offsets(df, name, ylim):
-    fig, ax = plt.subplots()
-    if ylim is not None:
-        ax.set_ylim(ylim)
-    
-    ax.ticklabel_format(style='plain')
+    fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+
+    ax[0].ticklabel_format(style='plain')
+    ax[1].ticklabel_format(style='plain')
 
     for i in range(NODES_NO):
-        df.plot(x='row_number', y=labels[i], color=colors[i], label=f"Węzeł {i}", kind='scatter', s=5, ax=ax)
-    plt.title("Przesunięcie zegarów")
-    plt.xlabel("Numer testu")
-    plt.ylabel("Czas [us]")
-    plt.legend()
-    plt.tight_layout()
+        df.plot(x='row_number', y=labels[i], color=colors[i], label=f"Węzeł {i}", kind='scatter', s=5, ax=ax[0])
+    # plt.title("Przesunięcie zegarów")
+    ax[0].set_xlabel("Numer testu")
+    ax[0].set_ylabel("Czas [us]")
+    ax[0].legend()
+
+    ax[1].set_ylim(ylim)
+
+    for i in range(NODES_NO):
+        df.plot(x='row_number', y=labels[i], color=colors[i], label=f"Węzeł {i}", kind='scatter', s=5, ax=ax[1])
+    # plt.title("Przesunięcie zegarów")
+    ax[1].set_xlabel("Numer testu")
+    ax[1].set_ylabel("Czas [us]")
+    ax[1].legend()
+
+    y_min, y_max = ylim
+    ax[0].axhline(y=y_min, color='red', linestyle='--', xmin=ax[0].get_xlim()[0], xmax=ax[0].get_xlim()[1])
+    ax[0].axhline(y=y_max, color='red', linestyle='--', xmin=ax[0].get_xlim()[0], xmax=ax[0].get_xlim()[1])
+
+    con = ConnectionPatch(xyA=(ax[0].get_xlim()[1], y_min), xyB=(ax[1].get_xlim()[0], y_min), coordsA="data", coordsB="data", axesA=ax[0], axesB=ax[1], color="red", linestyle='--')
+    fig.add_artist(con)
+    con = ConnectionPatch(xyA=(ax[0].get_xlim()[1], y_max), xyB=(ax[1].get_xlim()[0], y_max), coordsA="data", coordsB="data", axesA=ax[0], axesB=ax[1], color="red", linestyle='--')
+    fig.add_artist(con)
+
+    fig.tight_layout()
     plt.savefig(f'../../charts/mic_sync/{name}.png', dpi=300)
     plt.close()
 
@@ -27,7 +46,7 @@ def plot_stddevs(df):
     stddevs = {x: df.std()[x] for x in labels}
 
     plt.bar(*zip(*stddevs.items()))
-    plt.title("Odchylenia standardowe przesunięć")
+    # plt.title("Odchylenia standardowe przesunięć")
     plt.xlabel("Numer węzła")
     plt.ylabel("Odchylenie [us]")
     plt.tight_layout()
@@ -42,6 +61,5 @@ if __name__ == "__main__":
 
     df[labels] -= df[labels].mean()
 
-    plot_offsets(df, 'offsets', None)
-    plot_offsets(df, 'offsets_close', (-200, 200))
+    plot_offsets(df, 'offsets', (-150, 150))
     plot_stddevs(df)
--- src/python/data/mic_sync_dist/mic_sync_dist.py
+++ src/python/data/mic_sync_dist/mic_sync_dist.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
+from matplotlib.patches import ConnectionPatch
 
 def plot_dists(df, name, ylim, slope):
     fig, ax = plt.subplots()
@@ -30,6 +31,53 @@ def plot_dists(df, name, ylim, slope):
     plt.savefig(f'../../charts/mic_sync_dist/{name}.png', dpi=300)
     plt.close()
 
+def plot_dists_cmp(df, name, ylim, slope):
+    fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+
+    df_T = df.T.reset_index()
+    df_T.drop(df_T.tail(1).index, inplace=True)
+    df_T['index2'] = df_T['index']
+    df_T = df_T.astype(float)
+
+    df_T.plot(x='index', y='index2', ax=ax[0], label='y=x')
+    for y in range(df_T.shape[1]-3):
+        df_T.plot(x='index', y=y, color='red', kind='scatter', s=10, ax=ax[0])
+
+    # plt.title("Obliczone odległości")
+    ax[0].set_xlabel("Rzeczywista odległość")
+    ax[0].set_ylabel("Obliczona odległość")
+    ax[0].get_legend().remove()
+    ax[0].legend()
+
+    ax[1].set_ylim(ylim)
+
+    df_T.plot(x='index', y='index2', ax=ax[1], label='y=x')
+    if slope is not None:
+        df_T['index3'] = df_T['index'] * slope
+        df_T = df_T.astype(float)
+        df_T.plot(x='index', y='index3', ax=ax[1], label=f'y={slope}x')
+    for y in range(df_T.shape[1]-3):
+        df_T.plot(x='index', y=y, color='red', kind='scatter', s=10, ax=ax[1])
+
+    # plt.title("Obliczone odległości")
+    ax[1].set_xlabel("Rzeczywista odległość")
+    ax[1].set_ylabel("Obliczona odległość")
+    ax[1].get_legend().remove()
+    ax[1].legend()
+
+    y_min, y_max = ylim
+    ax[0].axhline(y=y_min, color='green', linestyle='--', xmin=ax[0].get_xlim()[0], xmax=ax[0].get_xlim()[1])
+    ax[0].axhline(y=y_max, color='green', linestyle='--', xmin=ax[0].get_xlim()[0], xmax=ax[0].get_xlim()[1])
+
+    con = ConnectionPatch(xyA=(ax[0].get_xlim()[1], y_min), xyB=(ax[1].get_xlim()[0], y_min), coordsA="data", coordsB="data", axesA=ax[0], axesB=ax[1], color="green", linestyle='--')
+    fig.add_artist(con)
+    con = ConnectionPatch(xyA=(ax[0].get_xlim()[1], y_max), xyB=(ax[1].get_xlim()[0], y_max), coordsA="data", coordsB="data", axesA=ax[0], axesB=ax[1], color="green", linestyle='--')
+    fig.add_artist(con)
+
+    fig.tight_layout()
+    plt.savefig(f'../../charts/mic_sync_dist/{name}.png', dpi=300)
+    plt.close()
+
 def clean(df):
     outliers = df > 10.0
     df[outliers] = float('nan')
@@ -41,13 +89,13 @@ if __name__ == '__main__':
     for i in range(4):
         df = pd.read_csv(f'mic_sync_dist_{i}.csv')
 
-        plot_dists(df, f'dists_{i}', None, None)
+        plot_dists_cmp(df, f'dists_{i}', (-.1, 3.5), 3)
+        plot_dists(df, f'dists_sep_{i}', None, None)
         plot_dists(df, f'dists_close_{i}', (-.1, 3.5), 3)
 
         df = pd.read_csv(f'mic_sync_dist_long_{i}.csv')
 
-        plot_dists(df, f'dists_long_{i}', None, None)
-        plot_dists(df, f'dists_close_long_{i}', (-.1, 6.5), None)
+        plot_dists_cmp(df, f'dists_long_{i}', (-.1, 6.5), None)
 
         df = clean(df)
         data = pd.DataFrame(df)
