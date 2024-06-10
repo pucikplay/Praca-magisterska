224525a788aa00339dc311b581ee0685a7f05dae
Gabriel Budziński <pucikplay@gmail.com>
2024-06-10 19:56:06 +0200

    More refactoring

--- src/python/data/mult_lat_1d/mult_lat.py
+++ src/python/data/mult_lat_1d/mult_lat.py
@@ -5,6 +5,7 @@ import re
 import pandas as pd
 import colorsys
 import matplotlib.colors as mc
+from matplotlib.patches import ConnectionPatch
 
 def plot_charts(df, point, nodes, ylim):
     df = df.reset_index()
@@ -12,25 +13,35 @@ def plot_charts(df, point, nodes, ylim):
     # print(point)
     # print(df)
 
-    fig = plt.figure()
-    ax1 = fig.add_subplot()
-    ax2 = ax1.twiny()
-    if ylim is not None:
-        ax1.set_ylim(ylim)
-        ax2.set_ylim(ylim)
+    fig = plt.figure(figsize=(12,6))
+    gs = fig.add_gridspec(1,3)
+    ax = [None, None, None]
+    ax[0] = fig.add_subplot(gs[:, 0])
+    ax[1] = fig.add_subplot(gs[:, 1:])
 
-    df.plot(x='index', y='x', kind='scatter', s=10, ax=ax1)
-    df.plot(x='index_2', y='x', kind='scatter', s=10, ax=ax2)
-    ax1.set_xlabel('Numer kalkulacji pozycji')
-    ax2.set_xlabel('Czas [s]')
-    ax1.set_ylabel('Obliczona pozycja nadajnika [m]')
-    plt.title(f'Punkt {point}, pozycja odbiornika')
-    fig.set_size_inches(8,6)
+    df.plot(x='index', y='x', kind='scatter', s=10, ax=ax[0])
+    ax[0].set_xlabel('Numer kalkulacji pozycji')
+    ax[0].set_ylabel('Obliczona pozycja nadajnika [m]')
+
+    ax[1].set_ylim(ylim)
+
+    df.plot(x='index', y='x', kind='scatter', s=10, ax=ax[1])
+    ax[1].set_xlabel('')
+    ax[1].set_ylabel('')
+    
+    y_min, y_max = ylim
+    ax[0].axhline(y=y_min, color='red', linestyle='--', xmin=ax[0].get_xlim()[0], xmax=ax[0].get_xlim()[1])
+    ax[0].axhline(y=y_max, color='red', linestyle='--', xmin=ax[0].get_xlim()[0], xmax=ax[0].get_xlim()[1])
+
+    con = ConnectionPatch(xyA=(ax[0].get_xlim()[1], y_min), xyB=(ax[1].get_xlim()[0], y_min), coordsA="data", coordsB="data", axesA=ax[0], axesB=ax[1], color="red", linestyle='--')
+    fig.add_artist(con)
+    con = ConnectionPatch(xyA=(ax[0].get_xlim()[1], y_max), xyB=(ax[1].get_xlim()[0], y_max), coordsA="data", coordsB="data", axesA=ax[0], axesB=ax[1], color="red", linestyle='--')
+    fig.add_artist(con)
+
+    # plt.title(f'Punkt {point}, pozycja odbiornika')
     fig.set_dpi(600)
-    if ylim is not None:
-        fig.savefig(f'../../charts/mult_lat_1d/position_{point}_{nodes}_close.png')
-    else:
-        fig.savefig(f'../../charts/mult_lat_1d/position_{point}_{nodes}.png')
+    fig.tight_layout()
+    fig.savefig(f'../../charts/mult_lat_1d/position_{point}_{nodes}.png')
     plt.close()
 
 def get_cmap(n, name='hsv'):
@@ -83,43 +94,43 @@ def plot_multiple(df_dict, lims, nodes, mean=False):
 # 2: [0.2]
 # 3: [-0.2]
 
-# if __name__ == '__main__':
-#     lims = {'[0]_4': (-0.04, 0.02),
-#             '[-0.5]_4': (-0.65, -0.4),
-#             '[0.5]_4': (0.45, 0.55),
-#             '[-0.25]_4': (-0.4, -0.2),
-#             '[0.25]_4': (0.1, 0.4),
-#             "[\'oscilate\']_4": None,
-#             '[0]_2': (0.0, 0.1),
-#             '[-0.5]_2': (-0.65, -0.5),
-#             '[0.5]_2': (0.5, 0.53),
-#             '[-0.25]_2': (-0.3, -0.1),
-#             '[0.25]_2': (0.28, 0.4),
-#             "[\'oscilate\']_2": None}
-#     directory = os.fsencode('.')
-#     for file in os.listdir(directory):
-#         filename = os.fsdecode(file)
-#         if filename.endswith(".csv"):
-#             df = pd.read_csv(filename)
-#             point = re.findall('\[.*\]',filename)[0]
-#             nodes = filename[filename.rfind('_')+1:-4]
-#             plot_charts(df, point, nodes, None)
-#             plot_charts(df, point, nodes, lims[f'{point}_{nodes}'])
-
 if __name__ == '__main__':
-    nodes_num = ['2', '4']
-    df_dict = {num: {} for num in nodes_num}
+    lims = {'[0]_4': (-0.033, -0.007),
+            '[-0.5]_4': (-0.65, -0.4),
+            '[0.5]_4': (0.49, 0.56),
+            '[-0.25]_4': (-0.4, -0.2),
+            '[0.25]_4': (0.12, 0.37),
+            "[\'oscilate\']_4": None,
+            '[0]_2': (0.0, 0.09),
+            '[-0.5]_2': (-0.61, -0.53),
+            '[0.5]_2': (0.509, 0.521),
+            '[-0.25]_2': (-0.25, -0.125),
+            '[0.25]_2': (0.29, 0.38),
+            "[\'oscilate\']_2": None}
     directory = os.fsencode('.')
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
         if filename.endswith(".csv"):
             df = pd.read_csv(filename)
-            df = df[df > -2]
-            df = df[df < 2]
-            point = re.findall('\[.*\]', filename)[0] 
+            point = re.findall('\[.*\]',filename)[0]
             nodes = filename[filename.rfind('_')+1:-4]
-            df_dict[nodes][point] = df
+            # plot_charts(df, point, nodes, None)
+            plot_charts(df, point, nodes, lims[f'{point}_{nodes}'])
 
-    for num in nodes_num:
-        plot_multiple(df_dict[num], {'xlim': (-0.7, 0.7), 'ylim': (-0.2, 0.2)}, num)
-        plot_multiple(df_dict[num], {'xlim': (-0.7, 0.7), 'ylim': (-0.2, 0.2)}, num, mean=True)
+# if __name__ == '__main__':
+#     nodes_num = ['2', '4']
+#     df_dict = {num: {} for num in nodes_num}
+#     directory = os.fsencode('.')
+#     for file in os.listdir(directory):
+#         filename = os.fsdecode(file)
+#         if filename.endswith(".csv"):
+#             df = pd.read_csv(filename)
+#             df = df[df > -2]
+#             df = df[df < 2]
+#             point = re.findall('\[.*\]', filename)[0] 
+#             nodes = filename[filename.rfind('_')+1:-4]
+#             df_dict[nodes][point] = df
+
+#     for num in nodes_num:
+#         plot_multiple(df_dict[num], {'xlim': (-0.7, 0.7), 'ylim': (-0.2, 0.2)}, num)
+#         plot_multiple(df_dict[num], {'xlim': (-0.7, 0.7), 'ylim': (-0.2, 0.2)}, num, mean=True)
--- src/python/data/position/position_charts.py
+++ src/python/data/position/position_charts.py
@@ -1,5 +1,91 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+from matplotlib.patches import ConnectionPatch
+
+def plot_charts_cmp(n,m):
+    ylims = [(-0.76,-0.44),(72.2,72.65),(74.45,74.64)]
+    ylims2 = [(-13.4,-12.85),(-11.5,-11.3)]
+    fontsize = 12
+    for i in range(n,m):
+        positions = []
+        with open(f'position_{i}.csv') as f:
+            for line in f:
+                pos = float(line)
+                positions.append(pos)
+            f.close()
+    
+        x = [d for d in range(len(positions))]
+        y = positions
+        ylim = ylims[i-2]
+
+        fig = plt.figure(figsize=(12,6))
+        gs = fig.add_gridspec(2,3)
+        ax = [None, None, None]
+        ax[0] = fig.add_subplot(gs[:, 0])
+        if i == 2:
+            ax[1] = fig.add_subplot(gs[:, 1:])
+        else:
+            ax[1] = fig.add_subplot(gs[0, 1:])
+            ax[2] = fig.add_subplot(gs[1, 1:])
+        
+        ax[0].scatter(x, y, s=10)
+        ax[0].set_xlabel('Numer kalkulacji pozycji', size=fontsize)
+        ax[0].set_ylabel('Obliczona pozycja nadajnika [m]', size=fontsize)
+        ax[0].tick_params(axis='x', labelsize=fontsize)
+        ax[0].tick_params(axis='y', labelsize=fontsize)
+
+        if i == 2:
+            ax[1].set_ylim(ylim)
+
+            ax[1].scatter(x, y, s=10)
+            ax[1].set_xlabel('')
+            ax[1].set_ylabel('')
+            ax[1].tick_params(axis='x', labelsize=fontsize)
+            ax[1].tick_params(axis='y', labelsize=fontsize)
+
+        if i != 2:
+            ylim2 = ylims2[i-3]
+
+            ax[1].set_ylim(ylim)
+
+            ax[1].scatter(x, y, s=10)
+            ax[1].set_xlabel('')
+            ax[1].set_ylabel('')
+            ax[1].tick_params(axis='x', labelsize=fontsize)
+            ax[1].tick_params(axis='y', labelsize=fontsize)
+
+            ax[2].set_ylim(ylim2)
+
+            ax[2].scatter(x, y, s=10)
+            ax[2].set_xlabel('')
+            ax[2].set_ylabel('')
+            ax[2].tick_params(axis='x', labelsize=fontsize)
+            ax[2].tick_params(axis='y', labelsize=fontsize)
+        
+        y_min, y_max = ylim
+        ax[0].axhline(y=y_min, color='red', linestyle='--', xmin=ax[0].get_xlim()[0], xmax=ax[0].get_xlim()[1])
+        ax[0].axhline(y=y_max, color='red', linestyle='--', xmin=ax[0].get_xlim()[0], xmax=ax[0].get_xlim()[1])
+
+        con = ConnectionPatch(xyA=(ax[0].get_xlim()[1], y_min), xyB=(ax[1].get_xlim()[0], y_min), coordsA="data", coordsB="data", axesA=ax[0], axesB=ax[1], color="red", linestyle='--')
+        fig.add_artist(con)
+        con = ConnectionPatch(xyA=(ax[0].get_xlim()[1], y_max), xyB=(ax[1].get_xlim()[0], y_max), coordsA="data", coordsB="data", axesA=ax[0], axesB=ax[1], color="red", linestyle='--')
+        fig.add_artist(con)
+
+        if i != 2:
+            y_min, y_max = ylim2
+            ax[0].axhline(y=y_min, color='red', linestyle='--', xmin=ax[0].get_xlim()[0], xmax=ax[0].get_xlim()[1])
+            ax[0].axhline(y=y_max, color='red', linestyle='--', xmin=ax[0].get_xlim()[0], xmax=ax[0].get_xlim()[1])
+
+            con = ConnectionPatch(xyA=(ax[0].get_xlim()[1], y_min), xyB=(ax[2].get_xlim()[0], y_min), coordsA="data", coordsB="data", axesA=ax[0], axesB=ax[2], color="red", linestyle='--')
+            fig.add_artist(con)
+            con = ConnectionPatch(xyA=(ax[0].get_xlim()[1], y_max), xyB=(ax[2].get_xlim()[0], y_max), coordsA="data", coordsB="data", axesA=ax[0], axesB=ax[2], color="red", linestyle='--')
+            fig.add_artist(con)
+
+        fig.set_dpi(600)
+        fig.tight_layout()
+        fig.savefig(f'../../charts/position/position_{i}.png')
+        plt.close()
 
 def plot_charts(n):
     for i in range(n):
@@ -15,18 +101,20 @@ def plot_charts(n):
 
         fig = plt.figure()
         ax1 = fig.add_subplot()
-        ax2 = ax1.twiny()
+        # ax2 = ax1.twiny()
 
-        ax1.scatter(x, y, s=3)
-        ax2.scatter([d/2 for d in x], y, s=10)
+        ax1.scatter(x, y, s=10)
+        # ax2.scatter([d/2 for d in x], y, s=10)
         ax1.set_xlabel('Numer kalkulacji pozycji')
-        ax2.set_xlabel('Czas [s]')
+        # ax2.set_xlabel('Czas [s]')
         ax1.set_ylabel('Obliczona pozycja nadajnika [m]')
         # fig.suptitle(f'Test {i}, pozycja')
         # plt.title(f'Test {i}, pozycja odbiornika')
         fig.set_size_inches(10,4)
         fig.set_dpi(600)
+        fig.tight_layout()
         fig.savefig(f'../../charts/position/position_{i}.png')
 
 if __name__ == '__main__':
     plot_charts(5)
+    plot_charts_cmp(2,5)
